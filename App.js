089f869b0ae2386d4ade080a86ae9de86806c39f
mariana0pachon <mariana0pachon@gmail.com>
2019-11-21 00:25:08 -0500

    setup parcel-react

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from '~components/Navbar';
+import Home from '~components/Home';
+import AboutMe from '~components/AboutMe';
+import MyWork from '~components/MyWork';
+import MyMethodology from '~components/MyMethodology';
+import Contact from '~components/Contact';
+
+const App = () => <>
+hello world
+</>;
+
+export default App;
